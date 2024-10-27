884597d46f685515fde5d6c2c2c4275c12c5fae5
CKShetty4 <ckshetty004@gmail.com>
2024-10-27 19:15:26 +0530

    add : css and game modes

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,14 +1,17 @@
 import { useContext } from "react";
 import Board from "./components/board";
+import Modal from "./components/modal";
+import Start from "./components/start";
 import { GameContext } from "./context/GameContext";
 
 function App() {
-  // const { screen } = useContext(GameContext);
+  const { screen } = useContext(GameContext);
   return (
     <div className="App">
       <div className="container">
-        <Board />
+        {screen === "start" ? <Start /> : <Board />}
       </div>
+      <Modal />
     </div>
   );
 }
