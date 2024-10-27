a78f608b048959ef230f3deba9072de9e03b7d44
CKShetty4 <ckshetty004@gmail.com>
2024-10-27 18:16:55 +0530

    add: assets

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Oicon from "../src/components/icons/Oicon";
+import Xicon from "../src/components/icons/Xicon";
 
 function App() {
   return (
     <div className="App">
+      <Xicon />
       <header className="App-header">
-        This is a Tic Tac Toe game project.
+      
+      
+        This is a Tic Tac Toe game project modified.
       </header>
+      <Oicon />
     </div>
   );
 }
